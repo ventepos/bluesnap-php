72388e4b173848195a23600faa1b8623b1bf4d08
Patrick McCarren <patrick@mmsc.io>
2017-12-12 15:48:29 -0500

    feat: laravel ServiceProvider support

---
+++ config/bluesnap.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'env' => env('BLUESNAP_ENV', 'sandbox'), // 'sandbox' || 'production'
+    'api-key' => env('BLUESNAP_API_KEY'),
+    'api-token' => env('BLUESNAP_API_TOKEN'),
+    'cse-key' => env('BLUESNAP_CSE_KEY'),
+];
