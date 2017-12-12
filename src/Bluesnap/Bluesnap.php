00f063bd1ff9f20b6380ed886653b4c58a10ac48
Patrick McCarren <patrick@mmsc.io>
2017-12-12 15:48:06 -0500

    feat: add CSE Key

--- src/Bluesnap/Bluesnap.php
+++ src/Bluesnap/Bluesnap.php
@@ -7,13 +7,15 @@ class Bluesnap
     private static $environment;
     private static $api_key;
     private static $password;
+    private static $cse_key = null;
     private static $base_url;
 
-    public static function init($environment, $api_key, $password)
+    public static function init($environment, $api_key, $password, $cse_key = null)
     {
         self::$environment = $environment;
         self::$api_key = $api_key;
         self::$password = $password;
+        self::$cse_key = $cse_key;
 
         if ($environment === 'production')
         {
@@ -23,6 +25,8 @@ class Bluesnap
         {
             self::$base_url = 'https://sandbox.bluesnap.com/services/2/';
         }
+
+        return new Static();
     }
 
     public static function getCredentials()
@@ -33,6 +37,11 @@ class Bluesnap
         }
 
         return [ self::$api_key, self::$password ];
+    }
+
+    public static function getCseKey()
+    {
+        return static::$cse_key;
     }
 
     public static function getEnvironment()
